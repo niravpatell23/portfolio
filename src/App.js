5cdd21ffea87503f75e025a9a592290d8f6122df
Nirav Patel <nnirav.patel7@gmail.com>
2023-06-16 01:12:19 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Exp from "./components/Exp";
+import Home from "./components/Home";
+import NavBar from "./components/NavBars";
+import PortFolio from "./components/PortFolio";
+import Social from "./components/Social";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavBar />
+      <Home />
+      <Social />
+      <About />
+      <PortFolio />
+      <Exp />
+      <Contact />
     </div>
   );
 }
