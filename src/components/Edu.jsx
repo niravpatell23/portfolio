4a1e2e5f07b30db3838017726a51c99456cf9037
Nirav Patel <nnirav.patel7@gmail.com>
2023-06-24 23:22:56 -0400

    Education section

---
+++ src/components/Edu.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Edu = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800
+       to-black text-white "
+    >
+      <div
+        className=" max-w-screen-lg p-4 mx-auto flex flex-col
+         justify-center w-full h-full"
+      >
+        <div className="">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Education
+          </p>
+        </div>
+        <p className=" text-xl mt-20 ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio,
+          consequatur ad totam dolorum temporibus ex earum cupiditate omnis id
+          officia provident deserunt nemo praesentium vero corporis adipisci
+          molestias ratione repudiandae!
+        </p>
+        <br />
+        <p className="text-xl">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia, at sit
+          dolorum, nam placeat nesciunt temporibus illum tempore quis harum
+          eaque obcaecati qui, veritatis excepturi deserunt accusantium ab
+          officia dolor.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Edu;
